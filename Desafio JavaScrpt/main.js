0e69f01fbb0c943668cd99951aa16b5306f66663
Vinícius Carvalho <121031707+vini120889@users.noreply.github.com>
2023-11-09 12:37:09 -0300

    Add files via upload

---
+++ Desafio JavaScrpt/main.js
@@ -0,0 +1,79 @@
+//1º exercicio
+//alert('Hello World!');
+
+//2º exercicio
+// let num_1 = 3;
+// let num_2 = 5;
+// let sum = Number(num_1 + num_2);
+
+// alert(`A soma entre ${num_1} e ${num_2} é: ${sum}`)
+
+//3º exercicio
+//  let num = 'abra';
+
+//  if (typeof num === 'number') {
+//    alert("É um número");
+//  } else {
+//    alert("Não é um número");
+//  }
+
+//4º exercicio
+//  let str = 'abra';
+
+//  if(typeof str === 'string'){
+//    alert('É uma string!')
+//  } else {
+//    alert('Não é uma string!')
+//  }
+
+//5º exercicio
+// let booleano = 'a';
+
+// if(typeof booleano === 'boolean'){
+//   alert('É um booleano!')
+// } else {
+//   alert('Não é um booleano!')
+// }
+
+//6º exercicio
+// let num_1 = 11;
+// let num_2 = 4;
+
+// let sub = Number(num_1 - num_2);
+
+// alert(`O resultado da subtração de ${num_1} e ${num_2} é: ${sub}`)
+
+//7º exercicio
+// let num_1 = 13;
+// let num_2 = 3;
+
+// let multi = Number(num_1 * num_2);
+
+// alert(`O resultado da multiplicação de ${num_1} e ${num_2} é: ${multi}`)
+
+//8º exercicio
+// let num_1 = 32;
+// let num_2 = 4;
+// let div = Number(num_1 / num_2);
+
+// alert(`O resultado da divisão de ${num_1} por ${num_2} é: ${div}`)
+
+//9º exercicio
+// let number = Number(4);
+// let par = (number % 2) == 0;
+
+// if(par){
+//   alert(`O número ${number} é par!`)
+// } else{
+//   alert(`O número ${number} é não é par!`)
+// }
+
+//10º exercicio
+// let number = Number(4);
+// let impar = (number % 2) != 0;
+
+// if(impar){
+//   alert(`O número ${number} é impar!`)
+// } else {
+//   alert(`O número ${number} não é impar!`)
+// }
